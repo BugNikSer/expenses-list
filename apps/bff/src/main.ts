fe5850536b8ba9f23b744e9ede1a615030a2e825
Nikita Bugrov <dustinman@mail.ru>
2024-12-30 18:34:38 +0300

    debug

--- apps/bff/src/main.ts
+++ apps/bff/src/main.ts
@@ -6,10 +6,16 @@ import environment from '../environment';
 const envVars = environment();
 
 async function bootstrap() {
+  console.log('====================== bootstrap')
   const app = await NestFactory.create(AppModule);
+  console.log('====================== app created')
   app.enableCors();
-  const trpc = app.get(TrpcRouter)
+  console.log('====================== cors enabled')
+  const trpc = app.get(TrpcRouter);
+  console.log('====================== trpc got')
   await trpc.applyMiddleware(app);
+  console.log('====================== middleware applied')
   await app.listen(envVars.port);
+  console.log('====================== listening port')
 }
 bootstrap();
