6a3aae6b651bde7aa3596aa5e738a6a3b7a10b67
Nikita Bugrov <dustinman@mail.ru>
2024-12-30 18:38:20 +0300

    debug

--- apps/bff/src/app.module.ts
+++ apps/bff/src/app.module.ts
@@ -3,14 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { TrpcModule } from './trpc/trpc.module';
-import environment from '../environment';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-      load: [environment],
-    }),
+    ConfigModule.forRoot(),
     TrpcModule,
   ],
   controllers: [AppController],
