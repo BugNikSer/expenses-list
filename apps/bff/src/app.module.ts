1d00fa08b59455aa4a0da06ecde7a16b0dfa6d8d
Nikita Bugrov <dustinman@mail.ru>
2024-12-30 18:42:56 +0300

    debug

--- apps/bff/src/app.module.ts
+++ apps/bff/src/app.module.ts
@@ -3,10 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { TrpcModule } from './trpc/trpc.module';
+import environment from '../environment';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [environment],
+    }),
     TrpcModule,
   ],
   controllers: [AppController],
--- apps/bff/src/trpc/trpc.module.ts
+++ apps/bff/src/trpc/trpc.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { TrpcService } from './trpc.service';
 import { TrpcRouter } from './trpc.router';
+import { TrpcService } from './trpc.service';
 
 @Module({
   imports: [],
-  controllers: [],
   providers: [TrpcService, TrpcRouter],
 })
 export class TrpcModule {}
