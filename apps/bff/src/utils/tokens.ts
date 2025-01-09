d35569df10a8555b10868eee5b2ab0f34533893a
Nikita Bugrov <dustinman@mail.ru>
2025-01-09 18:33:11 +0300

    web restructured

--- apps/bff/src/utils/tokens.ts
+++ apps/bff/src/utils/tokens.ts
@@ -4,13 +4,19 @@ import jwt from 'jsonwebtoken';
 import config from './config';
 import { areaLogger } from './logger';
 
+const logger = areaLogger('token-utils');
+
 export const TOKEN_KEY = 'access-token';
-export const REFRESH_TOKEN_KEY = 'refresh-access-token';
+export const REFRESH_TOKEN_KEY = 'refresh-token';
 const HOUR = 60_000;
 const DAY = 86_400_000;
+const cookieOptions = {
+  httpOnly: true,
+  sameSite: 'strict',
+  secure: true,
+} as const;
 
 export const parseToken = (token: string) => {
-  const logger = areaLogger('parse-token');
   if (!config.authSecreteKey) {
     logger.error('Missing config.authSecreteKey');
     return null;
@@ -21,22 +27,17 @@ export const parseToken = (token: string) => {
     if (!data.userId) return null;
     return data.userId;
   } catch (e) {
-    return null
+    return null;
   }
 };
 
 export const generateTokens = (userId: number) => {
-  const logger = areaLogger('generate-token');
   if (!config.authSecreteKey) {
     logger.error('Missing config.authSecreteKey');
     return null;
   }
 
-  const data = {
-    userId,
-    ts: Number(new Date()),
-  }
-
+  const data = { userId, ts: Number(new Date()) };
   const token = jwt.sign(data, config.authSecreteKey, { expiresIn: HOUR });
   const refreshToken = jwt.sign(data, config.authSecreteKey, { expiresIn: DAY });
 
@@ -45,13 +46,13 @@ export const generateTokens = (userId: number) => {
 
 export const setTokenCookies = (
   res: CreateExpressContextOptions['res'],
-  tokens: {token: string, refreshToken: string}
+  tokens: { token: string, refreshToken: string },
 ) => {
-  res.cookie(TOKEN_KEY, tokens.token, { maxAge: DAY, httpOnly: true, sameSite: 'lax' });
-  res.cookie(REFRESH_TOKEN_KEY, tokens.refreshToken, { maxAge: DAY, httpOnly: true, sameSite: 'lax' });
-}
+  res.cookie(TOKEN_KEY, tokens.token, { maxAge: DAY, ...cookieOptions });
+  res.cookie(REFRESH_TOKEN_KEY, tokens.refreshToken, { maxAge: DAY, ...cookieOptions });
+};
 
 export const dropTokenCookies = (res: CreateExpressContextOptions['res']) => {
-  res.cookie(TOKEN_KEY, '', { expires: new Date(), httpOnly: true });
-  res.cookie(REFRESH_TOKEN_KEY, '', { expires: new Date(), httpOnly: true });
-}
+  res.cookie(TOKEN_KEY, '', { expires: new Date(), ...cookieOptions });
+  res.cookie(REFRESH_TOKEN_KEY, '', { expires: new Date(), ...cookieOptions });
+};
--- apps/web/src/app/ClientSide.tsx
+++ apps/web/src/app/ClientSide.tsx
@@ -4,19 +4,9 @@ import { useState } from "react"
 import { trpc } from "../utils/trpc"
 
 const ClientSide = () => {
-  const [login, setLogin] = useState('')
-  const [password, setPassword] = useState('')
   const [greeting, setGreeting] = useState('')
 
   const [expenses, setExpenses] = useState<{ value: number, label: string }[] | null>(null)
-
-  const loginFn = () => {
-    trpc.auth.login.mutate({ login, password }).then(response => {
-      console.log(response);
-      setLogin('');
-      setPassword('');
-    })
-  }
 
   const greetingFn = () => {
     trpc.auth.test.query().then(response => {
@@ -40,11 +30,7 @@ const ClientSide = () => {
 
   return (
     <div style={{ display: 'flex', flexDirection: 'column', gap: 4 }}>
-      <div style={{ display: 'flex', flexDirection: 'row', gap: 4 }}>
-        <input value={login} onChange={(ev) => setLogin(ev.target.value)} />
-        <input value={password} onChange={(ev) => setPassword(ev.target.value)} />
-        <button onClick={loginFn}>login</button>
-      </div>
+
       <div style={{ display: 'flex', flexDirection: 'row', gap: 4 }}>
         <button onClick={greetingFn}>test</button>
         <span>{greeting}</span>
